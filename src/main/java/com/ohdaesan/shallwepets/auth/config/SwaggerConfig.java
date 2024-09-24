527f495d448fe9aa3e455f900deb3043ca75c71f
juyeon99 <beive999@naver.com>
2024-09-24 17:57:43 +0900

    [FEAT] Swagger OpenAPI Bean 추가
    Swagger OpenAPI Bean 추가

--- src/main/java/com/ohdaesan/shallwepets/auth/config/SwaggerConfig.java
+++ src/main/java/com/ohdaesan/shallwepets/auth/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.ohdaesan.shallwepets.auth.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +24,20 @@ public class SwaggerConfig {
                 .group("api-v1")
                 .pathsToMatch(paths)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
     }
 }
