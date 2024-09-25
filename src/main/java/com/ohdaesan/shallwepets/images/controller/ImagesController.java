9812158c2e495b89b7722c8ce198eb8bd5a2bbb0
juyeon99 <beive999@naver.com>
2024-09-25 16:47:52 +0900

    [FEAT] AWS S3 이미지 업로드 기능 추가
    AWS S3 이미지 업로드 기능 추가

--- src/main/java/com/ohdaesan/shallwepets/images/controller/ImagesController.java
+++ src/main/java/com/ohdaesan/shallwepets/images/controller/ImagesController.java
@@ -1,11 +1,19 @@
 package com.ohdaesan.shallwepets.images.controller;
 
+import com.ohdaesan.shallwepets.global.ResponseDTO;
+import com.ohdaesan.shallwepets.images.service.S3Service;
+import com.ohdaesan.shallwepets.images.domain.dto.ImagesDTO;
 import com.ohdaesan.shallwepets.images.service.ImagesService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
 
 // Amazon S3 사용
 @Tag(name = "Images")
@@ -15,6 +23,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/images")
 public class ImagesController {
     private final ImagesService imagesService;
+    private final S3Service s3Service;
 
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseDTO> uploadFile(ImagesDTO imagesDTO) {
+        Map<String, String> map = null;
 
+        try {
+            map = s3Service.uploadFile(imagesDTO.getFile());
+
+            String imageUrl = map.get("imageUrl");
+            String imageOrigName = map.get("imageOrigName");
+            String imageSavedName = map.get("imageSavedName");
+
+            imagesDTO.setImageUrl(imageUrl);
+            imagesDTO.setImageOrigName(imageOrigName);
+            imagesDTO.setImageSavedName(imageSavedName);
+
+            imagesService.save(imagesDTO);
+
+            return ResponseEntity.ok().body(new ResponseDTO(200, "이미지 업로드 성공", imageUrl));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
