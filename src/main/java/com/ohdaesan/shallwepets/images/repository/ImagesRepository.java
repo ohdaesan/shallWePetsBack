582a94e590cbfb9681e6d46dd1939a4c8fcec720
juyeon99 <beive999@naver.com>
2024-10-08 12:14:25 +0900

    [FEAT] 포스트 번호로 관련 이미지들 페이징 처리하여 가져오는 기능 추가
    포스트 번호로 관련 이미지들 페이징 처리하여 가져오는 기능 추가

--- src/main/java/com/ohdaesan/shallwepets/images/repository/ImagesRepository.java
+++ src/main/java/com/ohdaesan/shallwepets/images/repository/ImagesRepository.java
@@ -1,6 +1,8 @@
 package com.ohdaesan.shallwepets.images.repository;
 
 import com.ohdaesan.shallwepets.images.domain.entity.Images;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -29,4 +31,15 @@ public interface ImagesRepository extends JpaRepository<Images, Long> {
             "WHERE pi.post.postNo = :postNo " +
             "ORDER BY i.createdDate DESC")
     List<Images> findPostImagesByPostNo(@Param("postNo") Long postNo);
+
+    @Query("SELECT i FROM Images i " +
+            "JOIN PostImages pi ON i.imageNo = pi.image.imageNo " +
+            "WHERE pi.post.postNo = :postNo " +
+            "UNION " +
+            "SELECT i FROM Images i " +
+            "JOIN ReviewImages ri ON i.imageNo = ri.image.imageNo " +
+            "JOIN Review r ON ri.review.reviewNo = r.reviewNo " +
+            "WHERE r.post.postNo = :postNo " +
+            "ORDER BY i.createdDate DESC")
+    Page<Images> findImagesByPostNoAndPageNo(Long postNo, Pageable pageable);
 }
